1d10c5f5812b9f3a76466d9d6c162cf2cdceccca
Ken Hsu <hken920@gmail.com>
2019-03-08 10:18:09 -0800

    added add.js, add.pug, contact.js & updated app.js file

--- Sprint1/myapp/app.js
+++ Sprint1/myapp/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var test_mysqlRouter = require('./routes/test_mysql')
 var addRouter = require('./routes/add')
+var contactRouter = require('./routes/contact')
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/test_mysql', test_mysqlRouter);
 app.use('/add', addRouter);
+app.use('/contact', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

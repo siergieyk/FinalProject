c5f91e5b146eb502ebc640759c40035148152222
Ken Hsu <hken920@gmail.com>
2019-03-13 21:24:33 -0700

    add.js & add.pug complete

--- Sprint1/myapp/routes/add.js
+++ Sprint1/myapp/routes/add.js
@@ -23,11 +23,21 @@ var con = mysql.createConnection({
 
 
   router.post('/', function(req, res, next){
-  var sql = "INSERT INTO  movies (title, director_name)"
-  + "VALUES (?, ?);"
+  var sql = "INSERT INTO  movies (title, director_name, actor_1_name, actor_2_name,"
+    + "actor_3_name, genres, movie_lang, content_rating, production_year, budget, imdb_score)"
+    + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
   var queParams = [
     req.body.title,
-    req.body.director
+    req.body.director,
+    req.body.actor1,
+    req.body.actor2,
+    req.body.actor3,
+    req.body.genre,
+    req.body.language,
+    req.body.content,
+    req.body.year,
+    req.body.budget,
+    req.body.rating
   ];
   con.query(sql, queParams, function(err, result, fields){
     if(err) console.log(err);

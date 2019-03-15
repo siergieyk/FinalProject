b45190e95356b77c4e05bcc088cfac1f6aa09794
Ken Hsu <hken920@gmail.com>
2019-03-14 21:27:40 -0700

    final route testings

--- Sprint1/myapp/routes/add.js
+++ Sprint1/myapp/routes/add.js
@@ -1,26 +1,7 @@
 var express = require('express');
-var mysql = require('mysql');
 var router = express.Router();
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "Movie_Database"
-});
-//
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-//   //Insert a record in the "customers" table:
-//   var sql = "INSERT INTO movies() VALUES ('Company Inc', 'Highway 37')";
-//   con.query(sql, function (err, result) {
-//     if (err) throw err;
-//     console.log("1 record inserted");
-//   });
-// });
-
-
+var con = require('../db');
 
   router.post('/', function(req, res, next){
   var sql = "INSERT INTO  movies (title, director_name, actor_1_name, actor_2_name,"
@@ -41,12 +22,10 @@ var con = mysql.createConnection({
   ];
   con.query(sql, queParams, function(err, result, fields){
     if(err) console.log(err);
-    console.log("Connected");
-    res.status(201).send('added');
+        res.render('addResult', { title: 'AddResult' });
   });
 
 })
-// con.end();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('add', { title: 'Add' });
